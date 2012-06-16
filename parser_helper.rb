4a6e978f7dc3c64b1a1871b1acefd678605711a6
Mohd Irtefa <irtefa1@illinois.edu>
2012-06-16 10:41:56 -0500

    fixed indentation: changed tab size and converted indentations to spaces

--- parser_helper.rb
+++ parser_helper.rb
@@ -1,33 +1,32 @@
 
-def add_start_tag(level,train,new_file)	
-	File.open(new_file,'a') do |f2|
-		if(level==0)
-			f2.puts "<#{train[2]} id= '#{train[1]}'>"	
-		elsif(level==1)
-		
-			f2.puts "<#{train[1]}>"
+def add_start_tag(level,train,new_file) 
+  File.open(new_file,'a') do |f2|
+    if(level==0)
+      f2.puts "<#{train[2]} id= '#{train[1]}'>" 
+    elsif(level==1)
+      f2.puts "<#{train[1]}>"
 
-			(2..train.length-1).each do|i|
-				f2 << "#{train[i]} "
-			end
-		
-			f2.puts ""
-		
-		elsif(level==2)
-			f2.puts "<#{train[1]}>#{train[2]}>"
-		end	
-	end
+      (2..train.length-1).each do|i|
+        f2 << "#{train[i]} "
+      end
+    
+      f2.puts ""
+    
+    elsif(level==2)
+      f2.puts "<#{train[1]}>#{train[2]}>"
+    end 
+  end
 end
 
 
 def add_end_tag(level,train,new_file)
-	File.open(new_file,'a') do |f2|
-		if(level==0)
-			f2.puts "</#{train[2]}>"
-		elsif(level==1)
-			f2.puts "</#{train[1]}>"
-		elsif(level==2)
-			f2.puts "</#{train[1]}>"
-		end
-	end
+  File.open(new_file,'a') do |f2|
+    if(level==0)
+      f2.puts "</#{train[2]}>"
+    elsif(level==1)
+      f2.puts "</#{train[1]}>"
+    elsif(level==2)
+      f2.puts "</#{train[1]}>"
+    end
+  end
 end
